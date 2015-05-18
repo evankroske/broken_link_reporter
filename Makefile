96379567320836d926ac76cdd3190535ef443809
Evan Kroske <evankroske@google.com>
2015-05-17 20:24:10 -0700

    still_broken: Add code to parse input file of broken links.

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 build/bin/still_broken: bin/still_broken.cc
 	mkdir -p build/bin
 	c++ -o build/bin/still_broken bin/still_broken.cc
+
+run: build/bin/still_broken
+	./build/bin/still_broken < input/broken_links
--- bin/still_broken.cc
+++ bin/still_broken.cc
@@ -1,6 +1,19 @@
 #include <cstdio>
+#include <memory>
+#include <string>
 
 int main(int argc, char **argv) {
-  printf("okay");
+  using std::string;
+  std::unique_ptr<char[]> buf(new char[1000]);
+  while (true) {
+    int status = scanf("%999s", buf.get());
+    if (status == EOF) {
+      break;
+    }
+    string source(buf.get());
+    scanf("%999s", buf.get());
+    string href(buf.get());
+    printf("%s -> %s\n", source.c_str(), href.c_str());
+  }
   return 0;
 }
